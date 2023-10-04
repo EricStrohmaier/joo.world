2c8854f49466120d75491d3d7ef51a2442656fe6
Eric Strohmaier <eric.strohmaier00@gmail.com!git config --global user.email eric.strohmaier00@gmail.comgit config --global user.email eric.strohmaier00@gmail.com!>
2023-10-04 07:11:42 -0400

    vercel errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,54 +1,9 @@
 import { FC } from "react";
 import Button from "./CommonUI/Button";
-import { useMetadata } from "../logic/utils/nostr/use-metadata";
-import {
-  NostrAccountConnection,
-  useNostrConnection,
-} from "../logic/utils/nostr/use-nostr-connection";
-import { getProfileDataFromMetaData } from "../logic/utils/helperFunctions";
 
 interface NavigationBarProps {}
 
 const NavigationBar: FC<NavigationBarProps> = () => {
-  const isLogged = sessionStorage.getItem("isLogged") === "true";
-  const { connection, setConnection } = useNostrConnection();
-  const pubkey = connection?.pubkey;
-  if (!pubkey) {
-    console.log("No pubkey");
-  }
-  const { metadata } = useMetadata({ pubkey: pubkey || "" });
-
-  async function connectToNostrExtension() {
-    if (window.nostr) {
-      try {
-        const pubkey = await window.nostr.getPublicKey();
-        return {
-          type: "nostr-ext",
-          pubkey,
-        } as NostrAccountConnection;
-      } catch (err) {
-        throw new Error("Permission to get public key rejected");
-      }
-    } else {
-      throw new Error(
-        "Couldn't find a nostr supporting extension in your browser"
-      );
-    }
-  }
-  async function login() {
-    try {
-      const connection = await connectToNostrExtension();
-      setConnection(connection);
-      sessionStorage.setItem("isLogged", "true");
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  const logout = () => {
-    setConnection(null);
-    sessionStorage.setItem("isLogged", "false");
-  };
-
   return (
     <div className="w-full  bg-white shadow-md fixed top-0 left-0 right-0 bg-opacity-50 z-[5] backdrop-filter backdrop-blur-lg">
       <div className="flex justify-center items-center z-10 px-3">
@@ -60,28 +15,16 @@ const NavigationBar: FC<NavigationBarProps> = () => {
           />
           <div className="flex font-bold text-md ">
             {/* maybe making a popup modal view here for the profile */}
-            {isLogged ? (
-              <div className="flex space-x-2">
-                <Button
-                  url={getProfileDataFromMetaData(metadata, pubkey || "").image}
-                  href={`/profile/${pubkey}`}
-                  style=" "
-                />
-                <button
-                  className=" bg-gray-100 border-2 border-gray-200 rounded-[10px] p-3"
-                  onClick={logout}
-                >
-                  Sign Out
-                </button>
-              </div>
-            ) : (
-              <button
-                onClick={login}
-                className="bg-gray-100 border-2 border-gray-200 rounded-[10px] p-3"
-              >
-                Sign In
+
+            <div className="flex space-x-2">
+              <button className=" bg-gray-100 border-2 border-gray-200 rounded-[10px] p-3">
+                Sign Out
               </button>
-            )}
+            </div>
+
+            <button className="bg-gray-100 border-2 border-gray-200 rounded-[10px] p-3">
+              Sign In
+            </button>
           </div>
         </div>
       </div>
--- src/logic/routes/index.tsx
+++ src/logic/routes/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home";
 import About from "../../pages/About";
