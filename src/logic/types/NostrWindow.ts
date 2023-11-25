14c7dad5310b6845002c1ffc4e846403d8ce04d1
EricStrohmaier <eric.strohmaier00@gmail.com>
2023-11-25 17:35:49 +0000

    vercel error stuff

--- src/logic/types/NostrWindow.ts
+++ src/logic/types/NostrWindow.ts
@@ -1,7 +1,6 @@
 import { Event, UnsignedEvent } from "nostr-tools"
-import { SignableDraftPlace } from "./Place"
 
 export type NostrWindow = {
   getPublicKey(): Promise<string>
-  signEvent(event: UnsignedEvent | SignableDraftPlace) : Promise<Event>
+  signEvent(event: UnsignedEvent ) : Promise<Event>
 }
